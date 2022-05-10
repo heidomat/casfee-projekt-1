e04e615413bedfdf1e483f1b24c63291463c1521
Heidi Germann <heidi.germann@outlook.com>
2022-05-10 12:08:38 +0200

    adjust run script eslint

--- source/public/scripts/index.js
+++ source/public/scripts/index.js
@@ -1,42 +1,41 @@
 /*
     Function styleSwitcher (Dark Mode / Color Mode)
  */
-function styleToggle () {
+function styleToggle() {
     if (document.body.classList.contains('dark-mode')) {
         document.body.classList.remove('dark-mode');
-    } else  {
+    } else {
         document.body.classList.add('dark-mode');
     }
-};
+}
 
 document.querySelector('#toggle-style').addEventListener('click', styleToggle);
 
 /* Show Task Form */
-function toggleView (viewToShow) {
+function toggleView(viewToShow) {
     switch (viewToShow) {
         case 'form':
-            document.getElementById("taskform").style.display='block';
-            document.getElementById("tasklist-wrapper").style.display='none';
+            document.getElementById("taskform").style.display = 'block';
+            document.getElementById("tasklist-wrapper").style.display = 'none';
             break;
         case 'list':
-            document.getElementById("taskform").style.display='none';
-            document.getElementById("tasklist-wrapper").style.display='block';
+            document.getElementById("taskform").style.display = 'none';
+            document.getElementById("tasklist-wrapper").style.display = 'block';
             break;
 
         default:
-            document.getElementById("taskform").style.display='none';
-            document.getElementById("tasklist-wrapper").style.display='block';
+            document.getElementById("taskform").style.display = 'none';
+            document.getElementById("tasklist-wrapper").style.display = 'block';
             break;
     }
 
-};
+}
 
 const viewButtons = document.querySelectorAll('button[data-view]');
 
 viewButtons.forEach(el => el.addEventListener('click', event => {
     toggleView(event.target.getAttribute("data-view"));
 }));
-
 
 
 /* Sorting Items
