640a4b8cd0d33bae9bd55ba130eab17a6cb10947
Heidi Germann <heidi.germann@outlook.com>
2022-05-17 15:17:43 +0200

    save tasks in localStorage and show items in list

--- source/public/scripts/index.js
+++ source/public/scripts/index.js
@@ -11,21 +11,21 @@ function styleToggle() {
 
 document.querySelector('#toggle-style').addEventListener('click', styleToggle);
 
-/* Show Task Form */
+/* Show Task Form or List */
 function toggleView(viewToShow) {
     switch (viewToShow) {
         case "form":
-            document.getElementById("taskform-wrapper").style.display = 'block';
-            document.getElementById("tasklist-wrapper").style.display = 'none';
+            document.querySelector("#taskform-wrapper").style.display = 'block';
+            document.querySelector("#tasklist-wrapper").style.display = 'none';
             break;
         case 'list':
-            document.getElementById("taskform-wrapper").style.display = 'none';
-            document.getElementById("tasklist-wrapper").style.display = 'block';
+            document.querySelector("#taskform-wrapper").style.display = 'none';
+            document.querySelector("#tasklist-wrapper").style.display = 'block';
             break;
 
         default:
-            document.getElementById("taskform-wrapper").style.display = 'none';
-            document.getElementById("tasklist-wrapper").style.display = 'block';
+            document.querySelector("#taskform-wrapper").style.display = 'none';
+            document.querySelector("#tasklist-wrapper").style.display = 'block';
             break;
     }
 
@@ -37,6 +37,81 @@ viewButtons.forEach(el => el.addEventListener('click', event => {
     toggleView(event.target.getAttribute("data-view"));
 }));
 
+/* Save Form Inputs to LocalStorage */
+
+const taskform = document.querySelector("#taskform-form");
+const tasklist = document.querySelector("#tasklist");
+
+const taskStorage = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : [];
+
+const showImportanceSymbols = (number) => {
+    let symbols = ``;
+    for (let i = 0; i < number; i++) {
+         symbols += '<i class="fa fa-bolt-lightning"></i>'
+    }
+    return symbols;
+}
+
+
+const listBuilder = (task) => {
+    const taskEntry = document.createElement('div');
+    taskEntry.classList.add('tasklist__entry', 'entry');
+    taskEntry.innerHTML = `
+                    <div class="entry__status">
+                        <input type="checkbox" name="entryStatus" id="entry-status-1"/>
+                        <label for="entry-status-1">${task.completed}</label>
+                    </div>
+
+                    <div class="entry__countdown">
+                        <i class="fa-regular fa-calendar"></i>
+                        <span class="countdown-text">${task.duedate}</span>
+                    </div>
+
+                    <div class="entry__title">
+                        <h3>${task.title}</h3>
+                    </div>
+
+                    <div class="entry__description">
+                        ${task.description}
+                    </div>
+
+                    <div class="entry__importance" data-importance="${task.importance}">
+                        ${showImportanceSymbols(task.importance)}
+                    </div>
+
+                    <div class="entry__edit">
+                        <button type="button" id="edit-task" class="btn btn--action action__edit" title="bearbeiten"
+                                data-uid="${task.uid}">
+                            <i class="fa fa-pen"></i>
+                        </button>
+                    </div>
+    `;
+    tasklist.appendChild(taskEntry);
+};
+
+
+function Task(taskTitle, taskDescription, taskImportanceInput, taskDueDate, taskCompleted) {
+    this.title = taskTitle
+    this.description = taskDescription;
+    this.importance = taskImportanceInput;
+    this.duedate = taskDueDate;
+    this.completed = taskCompleted;
+}
+
+taskform.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = taskform.elements;
+    const taskEntry = new Task(formData.taskTitle.value, formData.taskDescription.value, formData.taskImportanceInput.value, formData.taskDueDate.value, formData.taskCompleted.value);
+    taskStorage.push(taskEntry);
+    localStorage.setItem("tasks", JSON.stringify(taskStorage));
+    listBuilder(taskEntry);
+    toggleView('list');
+});
+
+
+taskStorage.forEach((task) => {
+    listBuilder(task);
+});
 
 /* Sorting Items
 
